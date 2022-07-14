4aa8f4ef868448d1b325e57bd076e1d6f56c1724
HoangKhang0410 <khongnoihailan@gmail.com>
2022-07-14 08:45:35 +0700

    create database and schema

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { createServer } from '@graphql-yoga/node';
+import express, { Express } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { typeDefs } from './schema/schema';
 import { resolvers } from './resolvers/Person';
 import { sequelize } from './db/db';
+
+const app: Express = express();
 
 const server = createServer({
     schema: makeExecutableSchema({
@@ -11,14 +16,19 @@ const server = createServer({
     }),
 });
 
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use('/graphql', server);
+
 async function main() {
     try {
         await sequelize.sync({ force: true });
         console.log('Connect to db successfully!!');
-        server.start();
+        app.listen(4000, () => console.log('Running a GraphQL API server at http://localhost:4000/graphql'));
     } catch (error) {
         console.log('Unable to connect to the database:', error);
     }
 }
 
-main();
+main().catch((error) => console.log(`Error starting server: ${error}`));
