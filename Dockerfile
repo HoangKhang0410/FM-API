75f8394a606dd99504a89808b012950e4051043f
HoangKhang0410 <khongnoihailan@gmail.com>
2022-07-21 12:27:15 +0700

    add docker-compose, Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node
+WORKDIR /usr/src/app
+COPY package.json .
+COPY package-lock.json .
+COPY tsconfig.json .
+RUN npm install
+COPY . .
+EXPOSE 4000
+CMD ["npm", "run", "dev"]
--- src/db/db.ts
+++ src/db/db.ts
@@ -4,10 +4,10 @@ import { Enroll } from '../models/Enroll';
 import { User } from '../models/User';
 
 export const sequelize = new Sequelize({
-    database: 'postgres',
+    database: process.env.DB_NAME,
     dialect: 'postgres',
-    username: 'khangne',
-    password: 'khangne_secret_password',
-    host: 'localhost',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST || 'localhost',
     models: [Class, User, Enroll],
 });
